3343f3e733d9539966d185a5c9e3168da65a785b
Edmund Home <edmundhome@hotmail.com>
2019-08-26 12:25:10 +0200

    afdf

--- Proyecto2/src/main/java/com/prueba/Proyecto2Application.java
+++ Proyecto2/src/main/java/com/prueba/Proyecto2Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Proyecto2Application {
 
 	public static void main(String[] args) {
+		System.out.println("Prueba");
 		SpringApplication.run(Proyecto2Application.class, args);
 	}
 
